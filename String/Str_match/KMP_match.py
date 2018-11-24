df03ae1874bb274166f88299afde6583ead9b9d0
JeffWb <32888893+JeffWb@users.noreply.github.com>
2018-11-24 11:50:13 +0800

    Update KMP_match.py

--- String/Str_match/KMP_match.py
+++ String/Str_match/KMP_match.py
@@ -11,16 +11,47 @@ def KMP(str1,str2):
 	len_str1 = len(str1)
 	len_str2 = len(str2)
 	table = next_table(str2)
-	cur = 0
+	cur = 0                 #cur of str1
 	while cur < len_str1 - len_str2 + 1:
 		if str1[cur,cur + len_str2] == str2:
 			return True
 		else:
 			for j in range(len_str2):
-				if str1[j] != str2[cur+j]:
-					cur += max(table[j - 1],1)
+				if str1[cur+j] != str2[j]:
+					if j == 0:
+						cur += 1       #first is not matched, move 1
+					else:
+						cur += table[j - 1]
+					break
 	return False
 	
 #next_table
 def next_table(str2):
-	#go to bed
+	table = [1]      #save mive steps intable,   1 means only str2's first char matched, move 1
+	pre = []         #save prefix
+	post = []	 #save postfix
+	for i in range(len(str2) - 1):
+		pre_c = pre.copy()
+		post_c = post.copy()
+		for j in range(i):
+			pre_c.append(p[:j+1])
+			pre_c.reverse()                  #reverse pre_c
+		for k in range(1,i+1):
+			post_c.append(p[k:i+1])
+		step = 0                                 #initialize step = 0
+		for h in range(len(pre_c)):
+			if pre_c[h] = post_c[h]:         #find longest son str of str2 matched 
+				step = i + 1 - len(pre_c[h])        #step = len(str) - longest matched str length
+				break
+			step = 1                         # no equal pre_str and post_str,   move 1
+		table.append(step)
+	return table
+
+if __name__ == '__main__':
+	print(naive_match("BBC ABCDAB ABCDABCDABDE", "ABCDABD"))
+	print(next_table("ABCDABD"))
+	print(KMP("BBC ABCDAB ABCDABCDABDE", "ABCDABD"))
+			
+			
+			
+			
